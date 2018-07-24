1e34f9292ed2f2880adf7bcce7ae6c9e30dff737
Shubhendu Vaid <Shubhendu_Vaid@epam.com>
2018-07-24 20:35:27 +0200

    Added Angular Project

---
+++ easy/findTheLeader/index.js
@@ -0,0 +1,23 @@
+let arr = [1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5];
+function solution(arr) {
+    let length = arr.length;
+    let currentValue = arr[0];
+    let counter = 0;
+    let maxCounter = 0;
+    let maxCounterValue = 0;
+    arr.forEach(element => {
+        if (currentValue !== element) {
+            if (counter > maxCounter) {
+                maxCounter = counter;
+                maxCounterValue = currentValue;
+            }
+            counter = 0;
+            currentValue = element;
+        }
+        counter++;
+    });
+    let obj = {};
+    obj[maxCounterValue] = maxCounter
+    return obj;
+}
+console.log(solution(arr));
