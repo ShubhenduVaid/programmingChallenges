aa222887b1184095a1f80e95ee2f97f4d18f090b
Shubhendu Vaid <Shubhendu_Vaid@epam.com>
2019-11-08 13:54:37 +0100

    Upgraded to angular v9 rc. IVY is here

--- src/app/app.preloadStrategy.ts
+++ src/app/app.preloadStrategy.ts
@@ -1,6 +1,8 @@
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CustomPreloadingStrategy implements PreloadingStrategy {
     preload(route: Route, load: Function): Observable<any> {
         return route.data && route.data.preload ? load() : of(null);
